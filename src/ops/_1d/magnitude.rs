730ad08786720b2e1b14e653189bb983fa4c9ac9
sigurd4 <sigurd.spangelo@gmail.com>
2025-02-06 08:24:15 +0100

    Improve magintude_squared

--- src/ops/_1d/magnitude.rs
+++ src/ops/_1d/magnitude.rs
@@ -5,26 +5,38 @@ use super::ArrayPartialMulDot;
 #[const_trait]
 pub trait ArrayPartialMagnitude<T, const N: usize>: ArrayPartialMulDot<T, N>
 {
-    fn try_magnitude_squared(self) -> Option<<T as Mul<T>>::Output>
+    fn try_magnitude_squared(&self) -> Option<<T as Mul<T>>::Output>
     where
         T: Mul<T, Output: AddAssign> + Copy;
-    async fn try_magnitude_squared_async(self) -> Option<<T as Mul<T>>::Output>
+    async fn try_magnitude_squared_async(&self) -> Option<<T as Mul<T>>::Output>
     where
         T: Mul<T, Output: AddAssign> + Copy;
 }
 
 impl<T, const N: usize> ArrayPartialMagnitude<T, N> for [T; N]
 {
-    fn try_magnitude_squared(self) -> Option<<T as Mul<T>>::Output>
+    fn try_magnitude_squared(&self) -> Option<<T as Mul<T>>::Output>
     where
         T: Mul<T, Output: AddAssign> + Copy
     {
-        self.try_mul_dot(self)
+        if N == 0
+        {
+            return None;
+        }
+
+        let mut y = self[0]*self[0];
+        let mut i = 1;
+        while i < N
+        {
+            y += self[i]*self[i];
+            i += 1
+        }
+        Some(y)
     }
-    async fn try_magnitude_squared_async(self) -> Option<<T as Mul<T>>::Output>
+    async fn try_magnitude_squared_async(&self) -> Option<<T as Mul<T>>::Output>
     where
         T: Mul<T, Output: AddAssign> + Copy
     {
-        self.try_mul_dot_async(self).await
+        self.try_mul_dot_async(*self).await
     }
 }
